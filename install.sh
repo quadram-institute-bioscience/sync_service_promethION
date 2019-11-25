8d3d91136ded10c0f0edb56ddef7520c83f314d5
thanhleviet <lethanhx2k@gmail.com>
2019-11-25 13:12:32 +0000

    Config files and install script for lsyncd on PromethION

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+#Clone and install lsyncd
+cd ~/Documents
+git clone https://github.com/axkibe/lsyncd.git
+cd lsyncd
+cmake .
+make
+sudo make install
+
+#Copy lsyncd rotation config into /etc/logrotate.d/
+sudo cp lsyncd /etc/logrotate.d/
+
+#Copy lsyncd.conf  into /etc/
+sudo cp lsyncd.conf /etc/
+
+#Copy lsyncd.service into /etc/systemd/system/
+sudo cp lsyncd.service /etc/systemd/system/
+#Enable lsyncd service
+sudo systemctl enable lsyncd.service
+#Start lsyncd service
+sudo systemctl start lsyncd.service
